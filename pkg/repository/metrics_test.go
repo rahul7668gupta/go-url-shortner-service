f4c0cf0d8059f7426f8586204244843027652b14
Rahul Gupta <rahul7668gupta@gmail.com>
2024-05-03 13:59:37 +0530

    added metrics and redirect repo tests

--- pkg/repository/metrics_test.go
+++ pkg/repository/metrics_test.go
@@ -1,9 +1,60 @@
-package repository
+package repository_test
 
 import (
+	"context"
 	"testing"
+
+	"github.com/go-redis/redismock/v9"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/constants"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/dto"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/logger"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/repository"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestShortnerRepo_GetMetrics(t *testing.T) {
+	ctx := context.Background()
+	loggerObj := logger.InitLogger()
+	redisClient, clientMock := redismock.NewClientMock()
+	redisMetricsData := []redis.Z{
+		{
+			Member: "https://www.infracloud.io",
+			Score:  5,
+		},
+		{
+			Member: "https://www.udemy.com",
+			Score:  3,
+		},
+		{
+			Member: "https://www.amazon.com",
+			Score:  1,
+		},
+	}
+	expectedMetrics := []dto.Metrics{
+		{
+			DomainName: "https://www.infracloud.io",
+			Count:      5,
+		},
+		{
+			DomainName: "https://www.udemy.com",
+			Count:      3,
+		},
+		{
+			DomainName: "https://www.amazon.com",
+			Count:      1,
+		},
+	}
 
+	// set redis client mock expectations
+	clientMock.ExpectZRevRangeWithScores(constants.DOMAIN_COUNTER, 0, 2).SetVal(redisMetricsData)
+
+	// Initialise Repo
+	shortnerRepo := repository.NewShortnerRepository(redisClient, loggerObj)
+	// Call test func
+	metrics, err := shortnerRepo.GetMetrics(ctx)
+	// Assert Errors
+	assert.Nil(t, err)
+	assert.Len(t, metrics, 3)
+	assert.Equal(t, expectedMetrics, metrics)
 }
--- pkg/repository/redirect_test.go
+++ pkg/repository/redirect_test.go
@@ -1,9 +1,30 @@
-package repository
+package repository_test
 
 import (
+	"context"
 	"testing"
+
+	"github.com/go-redis/redismock/v9"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/logger"
+	"github.com/rahul7668gupta/go-url-shortner/pkg/repository"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestShortnerRepo_GetOriginalUrlForShortCode(t *testing.T) {
+	shortCode := "abc"
+	testUrl := "https://www.infracloud.io"
+	ctx := context.Background()
+	loggerObj := logger.InitLogger()
+	redisClient, clientMock := redismock.NewClientMock()
 
+	// set redis client mock expectations
+	clientMock.ExpectGet(shortCode).SetVal(testUrl)
+
+	// Initialise Repo
+	shortnerRepo := repository.NewShortnerRepository(redisClient, loggerObj)
+	// Call test func
+	originalUrl, err := shortnerRepo.GetOriginalUrlForShortCode(ctx, shortCode)
+	// Assert Errors
+	assert.Nil(t, err)
+	assert.Equal(t, testUrl, originalUrl)
 }
